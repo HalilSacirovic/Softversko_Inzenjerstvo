4e7a327b1c2858bf5400a51a1a2ec7e8311c8b1c
HalilSacirovic <halilsacirovic03@gmail.com>
2024-12-07 20:44:22 +0100

    Product page setuping grid's  and sizes

--- my-app/src/pages/ProductPage/Product.js
+++ my-app/src/pages/ProductPage/Product.js
@@ -9,9 +9,24 @@ const Product = () => {
       <NavBar />
       <Container>
         <CategoriesNav />
-        <Grid container spacing={2}>
-          <Grid size={8}>NESTO</Grid>
-          <Grid>NESTO</Grid>
+        <Grid container spacing={1}>
+          <Grid size={8}>
+            <Box sx={{ backgroundColor: "blue", height: 500, display: "flex" }}>
+              {/* <Box sx={{ backgroundColor: "purple", height: 400 }}>TEST</Box>
+              <Box sx={{ backgroundColor: "PINK", height: 400 }}>AWAW</Box> */}
+              <Grid size={2} sx={{ backgroundColor: "yellow", height: 400 }}>
+                TEKS
+              </Grid>
+
+              <Grid size={10} sx={{ backgroundColor: "aqua", height: 400 }}>
+                TEKS
+              </Grid>
+            </Box>
+          </Grid>
+
+          <Grid size={4}>
+            <Box sx={{ backgroundColor: "red", height: 500 }}> tets</Box>
+          </Grid>
         </Grid>
       </Container>
     </Box>
